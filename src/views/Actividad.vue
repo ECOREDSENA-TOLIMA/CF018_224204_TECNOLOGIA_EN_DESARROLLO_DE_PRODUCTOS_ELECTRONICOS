6223d91aa0c1c035353e2f83212de405e1268e65
Sebastian Trujillo <129899570+strujilloa@users.noreply.github.com>
2023-06-22 14:50:27 -0500

    Actualización Ortogrfía

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -33,7 +33,7 @@
         .tarjeta.actividad.p-3
           .row.justify-content-around.align-items-center            
             .col-sm.mb-3.mb-sm-0
-              p.fw-bold.mb-0 ctividad didáctica arrastrar y soltar
+              p.fw-bold.mb-0 Actividad didáctica arrastrar y soltar
             .col-auto
               a.boton.boton--b(:href="obtenerLink('/actividades/AD2/actividad.html')" target="_blank")
                 span Realizar
